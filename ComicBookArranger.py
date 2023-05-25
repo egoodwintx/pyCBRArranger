347b1422c0a7e848b3eaf0011688df27078a606e
Ed Goodwin <ed.registration@gmail.com>
2023-05-25 13:05:46 -0500

    first commit

---
+++ ComicBookArranger.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+#
+# ComicBookArranger.py
+#
+# author : Ed Goodwin
+# date : 05.25.2023
+#
+
+import os
+import sys
+
+class ComicBookArranger:
+    def __init__(self, archive_name):
+        return None
+    
+if __name__ == "__main__":
+    cv = ComicViewer(sys.argv[1])
+    cv.run()
