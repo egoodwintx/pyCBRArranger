d7a5eb6bafc9a8186be9136166d10007cdd46a05
Ed Goodwin <ed.registration@gmail.com>
2023-06-04 01:06:35 -0500

    YAML Config Reader

--- ReadYAMLProperties.py
+++ ReadYAMLProperties.py
@@ -3,27 +3,46 @@ import yaml
 # Class to store information about user preference
 
 class Configuration:
-    # get CBRArranger config file
+    # set up default class members
     file_path = 'cbrProperties.yaml'
-
-
-
+    tempdirStr = ""
+    archivedirStr = ""
+    cb7 = ""
+    cbr = ""
+    cba = ""
+    cbz = ""
+    cbt = ""
 
     # Access configuration properties
-    def read_config(file_path):
+    def read_config(self, file_path):
         with open(file_path, 'r') as file:
             config = yaml.safe_load(file)
-        tempdirStr = config['tempdir']
-        archivedirStr = config['archivetype']
-        return config
+        self.tempdirStr = config['tempdir']
+        self.archivedirStr = config['archivetype']
+        
+        # define the compression types
+        self.cb7 = config["CB7"]
+        self.cba = config["CBA"]
+        self.cbt = config["CBT"]
+        self.cbz = config["CBZ"]
+        self.cbr = config["CBR"]
     
     # Return a configuration value
-    def get_property(property_name):
+    def get_property(self, property_name):
         match property_name:
             case "tempdirStr":
                 return self.tempdirStr
             case "archivedirStr":
                 return self.archivedirStr
+            case "compressorTypes":
+                return {
+                    "cbz" : self.cbz,
+                    "cba" : self.cba,
+                    "cbr" : self.cbr,
+                    "cbz" : self.cbz,
+                    "cbt" : self.cbt,
+                    "cb7" : self.cb7
+                }
             
     def __init__(self) -> None:
         self.read_config(self.file_path)
@@ -31,4 +50,3 @@ class Configuration:
 
 if __name__ == "__main__":
     cv = Configuration()
-    cv.run()
